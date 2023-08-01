556f333eb7c003a4fffcff9cabd45b56193b0cf6
Imhemed <emhmed33334@gmail.com>
2023-08-01 18:07:11 +0200

    completed the logic

--- script.js
+++ script.js
@@ -15,9 +15,16 @@ const buttonMulti = document.querySelector(".buttonMulti");
 const buttonDivistion = document.querySelector(".buttonDivistion");
 const buttonDot = document.querySelector(".buttonDot");
 const buttonEquals = document.querySelector(".buttonEquals");
-// state mangement
+const acButton = document.querySelector(".acButton");
+const cButton = document.querySelector(".cButton");
+const openParenthesesButton = document.querySelector(".openParenthesesButton");
+const closeParenthesesButton = document.querySelector(
+	".closeParenthesesButton"
+);
+
+//  showing the current numbers selected
 const textArea = document.querySelector(".textArea");
-const body = document.querySelector("body");
+
 // the string numbers
 let firstNumber = "";
 let secondNumber = "";
@@ -25,71 +32,142 @@ let secondNumber = "";
 let result = 0;
 // mangement of the currnet state
 let state = 1;
+let operation = "";
+let parentheses = 0;
 
 // adding the event lisernes to ther buttons
-button1.addEventListener("click", (e) => {
-	addnumbers("1");
+// function buttons
+
+acButton.addEventListener("click", (e) => {
+	firstNumber = "";
+	secondNumber = "";
+	result = 0;
+	state = 1;
+	operation = "";
+	parentheses = 0;
+	return firstNumber, secondNumber, result, state, operation, parentheses;
+});
+cButton.addEventListener("click", (e) => {
+	if (state == 1) {
+		firstNumber = "";
+	} else {
+		secondNumber = "";
+	}
+});
+openParenthesesButton.addEventListener("click", (e) => {
+	parentheses = parentheses + 1;
+	if (state == 1) {
+		firstNumber = firstNumber + " * ( ";
+		console.log(firstNumber);
+		return firstNumber;
+	}
+	if (state == 2) {
+		switch (secondNumber.slice(-1)) {
+			case "+":
+			case "-":
+			case "/":
+			case "*":
+				break;
+			default:
+				secondNumber = secondNumber + " * ( ";
+				return secondNumber;
+		}
+		return secondNumber;
+	}
+	console.log(firstNumber);
+	return parentheses;
+});
+closeParenthesesButton.addEventListener("click", (e) => {
+	parentheses = parentheses - 1;
+	addnumbers(")");
+	return parentheses;
 });
 
+// Numberd buttons
+button1.addEventListener("click", (e) => {
+	checkTheOperation();
+	addnumbers("1");
+});
 button2.addEventListener("click", (e) => {
+	checkTheOperation();
 	addnumbers("2");
 });
 
 button3.addEventListener("click", (e) => {
+	checkTheOperation();
 	addnumbers("3");
 });
 
 button4.addEventListener("click", (e) => {
+	checkTheOperation();
 	addnumbers("4");
 });
 
 button5.addEventListener("click", (e) => {
+	checkTheOperation();
 	addnumbers("5");
 });
 
 button6.addEventListener("click", (e) => {
+	checkTheOperation();
 	addnumbers("6");
 });
 
 button7.addEventListener("click", (e) => {
+	checkTheOperation();
 	addnumbers("7");
 });
 
 button8.addEventListener("click", (e) => {
+	checkTheOperation();
 	addnumbers("8");
 });
 
 button9.addEventListener("click", (e) => {
+	checkTheOperation();
 	addnumbers("9");
 });
 
 button0.addEventListener("click", (e) => {
+	checkTheOperation();
 	addnumbers("0");
 });
 
 // opereation section
 buttonPlus.addEventListener("click", (e) => {
-	body.dataset.operation = "+";
-	textArea.textContent = "";
-	return (state = 2);
+	if (parentheses > 0) {
+		addnumbers("+");
+	} else {
+		textArea.textContent = "";
+		return (state = 2), (operation = "+");
+	}
 });
 
 buttonMinos.addEventListener("click", (e) => {
-	body.dataset.operation = "-";
-	textArea.textContent = "";
-	return (state = 2);
+	if (parentheses > 0) {
+		addnumbers("-");
+	} else {
+		textArea.textContent = "";
+		return (state = 2), (operation = "-");
+	}
 });
 
 buttonMulti.addEventListener("click", (e) => {
-	body.dataset.operation = "x";
-	textArea.textContent = "";
-	return (state = 2);
+	if (parentheses > 0) {
+		addnumbers("*");
+	} else {
+		textArea.textContent = "";
+		return (state = 2), (operation = "x");
+	}
 });
 
 buttonDivistion.addEventListener("click", (e) => {
-	body.dataset.operation = "/";
-	textArea.textContent = "";
-	return (state = 2);
+	if (parentheses > 0) {
+		addnumbers("/");
+	} else {
+		textArea.textContent = "";
+		return (state = 2), (operation = "/");
+	}
 });
 
 buttonDot.addEventListener("click", (e) => {
@@ -98,12 +176,12 @@ buttonDot.addEventListener("click", (e) => {
 
 buttonEquals.addEventListener("click", (e) => {
 	if ((state = 1)) {
-		textArea.textContent = "ENTER THE SECOND NUMBER";
+		result = eval(firstNumber);
 	}
 	if ((state = 2)) {
-		number1 = Number(firstNumber);
-		number2 = Number(secondNumber);
-		switch (body.dataset.operation) {
+		number1 = eval(firstNumber);
+		number2 = eval(secondNumber);
+		switch (operation) {
 			case "+":
 				result = number1 + number2;
 
@@ -119,6 +197,9 @@ buttonEquals.addEventListener("click", (e) => {
 				break;
 		}
 		textArea.textContent = `${result}`;
+		firstNumber = `${result}`;
+		secondNumber = "";
+		operation = "";
 	}
 });
 
@@ -135,4 +216,20 @@ function addnumbers(number) {
 			textArea.textContent = `${secondNumber}`;
 			break;
 	}
+}
+function checkTheOperation() {
+	switch (state) {
+		case 2:
+			if (operation == "") {
+				state = 1;
+				firstNumber = "";
+				secondNumber = "";
+				return state, secondNumber, firstNumber;
+			}
+		case 1:
+			console.log(" state = 1");
+	}
+}
+function checkBeforeParenthsis(string) {
+	return string;
 }
